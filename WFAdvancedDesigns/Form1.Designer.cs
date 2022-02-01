283a9e39c14714658e1c5cc114c2b83a5afd1fe6
3n3a <3n3a@enea.software>
2022-02-01 21:23:12 +0100

    first shitpost

---
+++ WFAdvancedDesigns/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace WFAdvancedDesigns
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.title = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.title);
+            this.splitContainer1.Panel1MinSize = 75;
+            this.splitContainer1.Size = new System.Drawing.Size(800, 561);
+            this.splitContainer1.SplitterDistance = 75;
+            this.splitContainer1.SplitterWidth = 1;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Hipster Full", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(309, 29);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(188, 22);
+            this.title.TabIndex = 0;
+            this.title.Text = "Test Application";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(800, 561);
+            this.Controls.Add(this.splitContainer1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label title;
+    }
+}
+
---
+++ WFAdvancedDesigns/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAdvancedDesigns
+{
+    public partial class Form1 : Form
+    {
+        public FlowLayoutPanel flowLayout { get; set; }
+        public Random random { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            flowLayout = new FlowLayoutPanel()
+            {
+                Width = Width,
+                Height = Height,
+                BorderStyle = BorderStyle.FixedSingle,
+                Padding = new Padding(20)
+            };
+            random = new Random();
+            
+
+            for (int i = 0; i < 100; i++)
+            {
+                Color color = Color.FromArgb(random.Next(1, 255), random.Next(1, 255), random.Next(1, 255));
+                Label newLabel = new Label()
+                {
+                    Text = $"Number {i}",
+                    BackColor = color
+                };
+                flowLayout.Controls.Add(newLabel);
+            }
+
+            splitContainer1.Panel2.Controls.Add(flowLayout);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            title.Location = new Point()
+            {
+                X = splitContainer1.Panel1.Width / 2 - title.Width / 2,
+                Y = splitContainer1.Panel1.Height / 2 - title.Height / 2,
+            };
+
+            flowLayout.Width = splitContainer1.Panel2.Width;
+            flowLayout.Height = splitContainer1.Panel2.Height;
+        }
+    }
+}
